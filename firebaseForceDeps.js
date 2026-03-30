8b9536651185dd8d59aca4f20ebe71c66c483951
Phạm Thanh Nhân <210466144+techmoodbiz@users.noreply.github.com>
2026-03-30 16:27:29 +0700

    123

--- firebaseForceDeps.js
+++ firebaseForceDeps.js
@@ -1,7 +1,5 @@
 // Vercel can miss transitive deps when firebase-admin is loaded dynamically.
 // These imports force bundlers/tracers to include gaxios/gcp-metadata in the artifact.
-import "gaxios/build/src/index.js";
-import "gcp-metadata/build/src/index.js";
-import "google-auth-library/build/src/index.js";
+import "gaxios";
 
 export {};
